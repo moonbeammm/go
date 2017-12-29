f259be2b2ce4ff18084c005da47ea7a8b21912ee
sunguangxin <sunguangxin@bilibili.com>
2017-12-29 19:09:19 +0800

    添加打开数据库注释

--- src/app/app.go
+++ src/app/app.go
@@ -9,25 +9,33 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// path: /app/hello
+// path: /app
 func AppHelloServer(w http.ResponseWriter, req *http.Request) {
 	fmt.Printf("%s",req.URL)
+
+	// 打开mysql
 	db, err := sql.Open("mysql", "root:123456@tcp(47.52.250.43:3306)/sgx?parseTime=true")
     if err != nil{
         log.Fatal(err)
-    }
+	}
+	// 关闭mysql
 	defer db.Close()
+
+	// 打开sgx.T_Account这个表
 	_, err = db.Exec("CREATE TABLE IF NOT EXISTS sgx.T_Account(world varchar(50))")
     if err != nil{
         log.Fatalln(err)
 	}
 	
+	// 查询表
 	rows, err := db.Query("SELECT * FROM T_Account")
 	if err != nil {
 		fmt.Println("fetech data failed:", err.Error())
 		return
 	}
+	// 关闭表
 	defer rows.Close()
+
 	for rows.Next() {
 		var id int
 		var account, pwd, accesskey string
--- src/main/main.go
+++ src/main/main.go
@@ -14,3 +14,15 @@ func main() {
 	http.HandleFunc("/app",app.AppHelloServer)
 	log.Fatal(http.ListenAndServe(":8888",nil))
 }
+
+func developNewCommit1() {
+	fmt.Printf("developNewCommit1")
+}
+
+func developNewCommit2() {
+	fmt.Printf("developNewCommit2")
+}
+
+func developNewCommit3() {
+	fmt.Printf("developNewCommit3")
+}
